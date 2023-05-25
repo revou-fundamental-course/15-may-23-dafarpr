607f5108b0bcba02e0dc99d1d1e761d183c62701
DAFA RPR <61611130+dafarpr@users.noreply.github.com>
2023-05-25 08:10:44 +0700

    Fix

--- src/script.js
+++ src/script.js
@@ -26,7 +26,40 @@ function KelilingPP(){
     var p = document.getElementById("panjang-keliling").value;
     var l = document.getElementById("lebar-keliling").value;
 
-    var w = 2 * (p + l);
+    var w = 2 * p + 2 * l;
 
     document.getElementById("hasil-kelilingPP").innerHTML = "K = 2 x (P + L) <br> K = 2 x (" + p + " + " + l + ") <br> K = " + w;
+}
+
+//reset
+function resetLuas(){
+    document.getElementById("sisi-luas").value = "";
+    document.getElementById("hasil-luas").innerHTML = "";
+}
+
+function resetKeliling(){
+    document.getElementById("sisi-keliling").value = "";
+    document.getElementById("hasil-keliling").innerHTML = "";
+}
+function resetLuasPP(){
+    document.getElementById("panjang-luas").value = "";
+    document.getElementById("lebar-luas").value = "";
+
+    document.getElementById("hasil-luasPP").innerHTML = "";
+}
+
+function resetKelilingPP(){
+    document.getElementById("panjang-keliling").value = "";
+    document.getElementById("lebar-keliling").value = "";
+
+    document.getElementById("hasil-kelilingPP").innerHTML = "";
+}
+//cek input
+function checkInput(){
+    var s = document.getElementById("sisi-luas").value;
+
+    if(s == ""){
+        alert("Tidak Boleh Kosong");
+        return false;
+    }
 }
